a73f927550cfc1070923b85edda66cc4c919de3e
david <david.alvgar23@gmail.com>
2023-10-31 20:56:13 -0700

    Terminado

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const connectDB = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
 
@@ -9,10 +11,14 @@ connectDB()
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/api/noticias', require('./routes/newsRoutes'))
+app.use('/api/users', require('./routes/usersRoutes'))
 
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
