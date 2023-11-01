2b3088f54e1592be74cb496b5ac8de829f42a53f
david <david.alvgar23@gmail.com>
2023-10-31 20:39:38 -0700

    routes agregados

--- backend/controllers/newsControlles.js
+++ backend/controllers/newsControlles.js
@@ -12,6 +12,52 @@ const setNoticia = asyncHandler( async (req, res) => {
     res.status(201).json('Noticia creada correctamente')
 })
 
+const getNoticas = asyncHandler( async (req, res) => {
+    const noticias = await Noticia.find()
+    res.status(200).json(noticias)
+})
+
+const getNoticasById = asyncHandler( async (req, res) => {
+    const noticias = await Noticia.findById(req.params.id)
+    res.status(200).json(noticias)
+})
+
+const updateNoticia = asyncHandler(async (req, res) => {
+    try {
+      const updatedNoticia = await Noticia.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      ) 
+  
+      if (!updatedNoticia) {
+        res.status(404).json({ message: 'La noticia no fue encontrada' }) 
+      } else {
+        res.status(200).json(updatedNoticia) 
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error al actualizar la noticia' }) 
+    }
+  }) 
+
+const deleteNoticia = asyncHandler( async (req, res) => {
+
+    const noticia = await Noticia.findById(req.params.id)
+
+    if(!noticia){
+      res.status(404)
+      throw new Error ('La Tarea no fue encontrada')
+    }
+
+    noticia.deleteOne()
+
+    res.status(200).json('Noticia Borrada')
+})
+
 module.exports = {
-    setNoticia
+    setNoticia,
+    getNoticas,
+    getNoticasById, 
+    updateNoticia,
+    deleteNoticia
 }
--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {setNoticia} = require('../controllers/newsControlles')
+const {setNoticia, getNoticas, getNoticasById, deleteNoticia, updateNoticia} = require('../controllers/newsControlles')
 
 router.post('/', setNoticia)
-router.get('/', )
-router.get('/:id', )
+router.get('/', getNoticas )
+router.get('/:id', getNoticasById)
+router.put('/:id', updateNoticia)
+router.delete('/:id', deleteNoticia)
 
 module.exports = router
