b58515f89b60b902c052f81f853c042417312484
david <david.alvgar23@gmail.com>
2023-10-31 18:43:55 -0700

    bd conectada

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 
@@ -9,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/api/noticias', require('./routes'))
+app.use('/api/noticias', require('./routes/newsRoutes'))
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
